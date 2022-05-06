1621d13ae8b7cf7894bea7babbefe4b0ac882da7
Deyvison Vianna <99549533+Deyvinhoo@users.noreply.github.com>
2022-05-05 22:02:27 -0300

    "Incluindo deletar e buscar atendimento"

--- controllers/atendimentos.controller.js
+++ controllers/atendimentos.controller.js
@@ -18,7 +18,48 @@ const atendimentosController = {
             return res.status(400).json('Falha ao cadastrar atendimento');
         }
     }, 
-    
+    async deletarAtendimento(req, res) {
+        try {
+            const { id } = req.params;
+
+            await Atendimentos.destroy({
+                where: {
+                    id,
+                }
+            });
+
+            res.status(204).json("Atendimento Deletado com sucesso");
+        } catch (error) {
+
+        }
+    },
+    async buscarAtendimento(req, res) {
+        try {
+            const atendimentos = await Atendimentos.findAll({
+                include: [pacientes, psicologos]
+                
+            });
+            res.json(atendimentos);
+        } catch (error) {
+            console.error('Erro de busca');
+        }
+    },
+
+    async buscarAtendimentoId(req, res) {
+        try {
+            const { id } = req.params;
+            const atendimentos = await Atendimentos.findByPk(id,{
+                include: [pacientes, psicologos]
+            });
+            if (atendimentos == null) {
+                return res.status(404).json('Id não encontrado');
+            }
+            res.json(atendimentos);
+        } catch (error) {
+            console.error('Id não encontrado');
+            res.status(404);
+        }
+    }
 };
 
 module.exports = atendimentosController;
